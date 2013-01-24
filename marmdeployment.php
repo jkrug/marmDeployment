f69537fb97886346587fc0703f4c353b145ab5b9
Joscha Krug <krug@marmalade.de>
2013-01-24 23:16:02 +0100

    Extended code with some startupchecks

--- marmdeployment.php
+++ marmdeployment.php
@@ -11,9 +11,31 @@
 
 if (php_sapi_name() == "cli")
 {
-    $baseDir = (__DIR__)."/";
+    $baseDir = (__DIR__)."/../";
     include_once $baseDir.'marmdeployment.conf.php';
+    
+    // Check environment
+    $handle = fopen($baseDir.'environment.txt','r');
+    
+    if($handle == false)
+    {
+        die('Environemt not specified!');
+    }
+    
+    $env = fgets($handle);
+    fclose($handle);
+    
+    // Check if environment is configured
+    if(!is_array($conf[$env]))
+    {
+        die('Environment not configured');
+    }
+    
+    //Go to project root and deploy there
+    chdir($baseDir);
     system("git pull origin master");
+    
+    
     foreach ($modules as $module)
     {
         chdir($baseDir.$module['dir']);
@@ -23,5 +45,5 @@ if (php_sapi_name() == "cli")
 }
 else
 {
-    echo "no execution from web";
+    echo "No execution from web.";
 }
