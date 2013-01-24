05b0075d2f75d6fe2839bd140087a42b4c3aca7f
Joscha Krug <krug@marmalade.de>
2013-01-25 00:34:55 +0100

    Fixed directorycheck and added some helpful output

--- marmdeployment.php
+++ marmdeployment.php
@@ -38,12 +38,13 @@ if (php_sapi_name() == "cli")
     system("git pull origin " . $conf['project']['ref']);
     
     //Now deploy the
-    foreach ($conf['modules'] as $module)
+    foreach ($conf['modules'] as $name=>$module)
     {
+		echo "\nWork on $name\n";
         //Check if dir exists
-        if(!is_dir($baseDir.$conf['dir']))
+        if(!is_dir($baseDir.$module['dir']))
         {
-            mkdir($baseDir.$conf['dir'], 0755, true);
+            mkdir($baseDir.$module['dir'], 0755, true);
             chdir($baseDir.$module['dir']);
             system("git init");
             system("git pull origin ".$module['ref']);
@@ -52,6 +53,7 @@ if (php_sapi_name() == "cli")
         {
             chdir($baseDir.$module['dir']);
         }
+        
         // pull the specified ref
         system("git fetch origin -a");
         system("git checkout ".$module['ref']);
