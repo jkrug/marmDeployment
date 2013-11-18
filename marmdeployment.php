1044c5efa1bd8bc261e88963c65c649ef14fa8fb
Michael Wille <wille@marmalade.de>
2013-11-18 11:12:19 +0100

    add remote set-url, so we dont do it manually

--- marmdeployment.php
+++ marmdeployment.php
@@ -62,6 +62,7 @@ if (php_sapi_name() == "cli")
         else
         {
             chdir($baseDir.$module['dir']);
+            //system("git remote set-url origin ".$module['remote']);
         }
         
         // pull the specified ref
